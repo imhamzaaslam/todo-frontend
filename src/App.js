525fe9ca347867c8f451d6a6bd4f6f0f4cebbc20
Hamza Aslam <imhamzaaslam@gmail.com>
2023-09-05 18:00:58 +0500

    code push

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './styles/App.css';
+
+function App() {
+    const [todos, setTodos] = useState([]);
+    const [title, setTitle] = useState('');
+
+    useEffect(() => {
+        fetchTodos();
+    }, []);
+
+    const fetchTodos = async () => {
+        const response = await axios.get('http://localhost:5000/api/todos');
+        setTodos(response.data);
+    }
+
+    const addTodo = async () => {
+        if (title) {
+            const response = await axios.post('http://localhost:5000/api/todos', { title });
+            setTodos([...todos, response.data]);
+            setTitle('');
+        }
+    }
+
+    return (
+        <div>
+            <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Add todo..." />
+            <button onClick={addTodo}>Add</button>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo._id}>{todo.title}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;

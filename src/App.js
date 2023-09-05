95fd052b19afd8f82d9ff1426077882e7a9649ce
Hamza Aslam <imhamzaaslam@gmail.com>
2023-09-05 23:23:49 +0500

    alert added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
+import ErrorAlert from './components/ErrorAlert';
 import './styles/App.css';
 
 function App() {
     const [todos, setTodos] = useState([]);
     const [title, setTitle] = useState('');
+    const [errorMessage, setErrorMessage] = useState(null); 
 
     useEffect(() => {
         fetchTodos();
@@ -18,10 +20,15 @@ function App() {
     }
 
     const addTodo = async () => {
-        if (title) {
-            const response = await axios.post(process.env.REACT_APP_API_URL, { title });
-            setTodos([...todos, response.data]);
-            setTitle('');
+        try {
+            if (title) {
+                const response = await axios.post(process.env.REACT_APP_API_URL, { title });
+                setTodos([...todos, response.data]);
+                setTitle('');
+            }
+        } catch (error) {
+            alert('Error adding todo: ' + error.message); 
+            console.error("Error adding todo:", error);
         }
     }
 
@@ -31,14 +38,17 @@ function App() {
 
         try {
             const response = await axios.put(process.env.REACT_APP_API_URL + `/${id}`, updatedTodo);
-            setTodos(todos.map(t => t._id === id ? response.data : t));
+            setTodos(todos.map(todo => todo._id === id ? response.data : todo));
+            alert(errorMessage);
         } catch (error) {
+            setErrorMessage('Something went wrong!');
             console.error("Error updating todo", error);
         }
     }
 
     return (
         <div>
+            <ErrorAlert message={errorMessage} />
             <AddTodo title={title} setTitle={setTitle} addTodo={addTodo} />
             <TodoList todos={todos} toggleCompletion={toggleCompletion} />
         </div>

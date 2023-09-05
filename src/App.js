93fa1735572d1b144363e3a8eda5e7950644b8cd
Hamza Aslam <imhamzaaslam@gmail.com>
2023-09-05 19:47:29 +0500

    css update

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
 
     return (
         <div>
-            <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Add todo..." />
+            <input className='textfield' value={title} onChange={e => setTitle(e.target.value)} placeholder="Add todo..." />
             <button onClick={addTodo}>Add</button>
             <ul>
                 {todos.map(todo => (
